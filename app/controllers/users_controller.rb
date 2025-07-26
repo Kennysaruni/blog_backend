be3c8795eb71aacfc29a245cee06826e289d5fa6
Kennysaruni <kennylatimore4@gmail.com>
2025-07-26 21:39:37 +0300

    Completed backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: {users: UserSerializer.new(@users)}
+    render json: User.all
   end
 
   # GET /api/users/:id
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body
   belongs_to :post
-  belongs_to :user
+  belongs_to :user, serializer: UserSerializer
 end
